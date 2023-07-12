b51e6824f299a6b5d25b3e6e589d4ce778bd2422
Serenata2 <astg98@gmail.com>
2023-07-12 13:51:51 +0900

    11726

---
+++ 백준/11726.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+
+#define MAX_NUM 1000
+#define DIVISION_NUM 10007
+
+using namespace std;
+
+long long D[MAX_NUM + 1];
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	D[1] = 1;
+	D[2] = 2;
+
+	// 동적계획법이용
+	for (int i = 3; i <= N; i++) {
+		D[i] = (D[i - 1] + D[i - 2]) % DIVISION_NUM;
+	}
+
+	cout << D[N];
+
+	return 0;
+}

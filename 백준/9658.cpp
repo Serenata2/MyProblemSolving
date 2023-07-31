1245d6520edebcb5916580121e63c85234278e77
Serenata2 <astg98@gmail.com>
2023-07-31 23:21:30 +0900

    9658

---
+++ 백준/9658.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+#define MAX_NUM 1000
+
+using namespace std;
+
+// D[i] : i개의 돌을 받았을 때 이길 수 있으면 true, 아니면 false
+bool D[MAX_NUM + 1];
+
+int main() {
+	int N;
+	cin >> N;
+
+	// 초기화
+	D[2] = true;
+	D[4] = true;
+
+	for (int i = 5; i <= N; i++) {
+		if (!D[i - 1] || !D[i - 3] || !D[i - 4]) {
+			D[i] = true;
+		}
+	}
+
+	if (D[N]) {
+		cout << "SK";
+	}
+	else {
+		cout << "CY";
+	}
+
+	return 0;
+}

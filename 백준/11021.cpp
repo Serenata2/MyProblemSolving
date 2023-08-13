042a45294fa5d2b0f7d3c92fbcae71284f0dc0d8
Serenata2 <astg98@gmail.com>
2023-08-13 23:10:05 +0900

    11021

---
+++ 백준/11021.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    for (int i = 0; i < T; i++) {
+        int A, B;
+        cin >> A >> B;
+
+        cout << "Case #" << i + 1 << ": " << A + B << endl;
+    }
+
+    return 0;
+}

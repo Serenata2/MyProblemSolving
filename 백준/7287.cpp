f28190ddb31ea647e9e9e3cc62b423acbed526d2
Serenata2 <astg98@gmail.com>
2023-10-01 23:55:33 +0900

    7287

---
+++ 백준/7287.cpp
@@ -0,0 +1,27 @@
+// 문제 8980
+/*
+* 알 수 있는 것
+* 1. 만약 1번 마을에서 실을 수 있는 박스의 종류가 여러개이면
+*    가까운 곳에서 내리는 박스의 가치가 더 좋다
+*    why : 그래야지 보다 더 많은 선택 기회가 생길 수 있기 때문!!
+* 
+* 2. 
+* 
+* 
+* 
+*/
+
+
+#include <iostream>
+#include <algorithm>
+
+#define MAX_NUM 10000
+
+using namespace std;
+
+int main() 
+{
+    
+
+    return 0;
+}
